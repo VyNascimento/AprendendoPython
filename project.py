268cba093ed7db77f42d94a63ef940442dad2549
Vyctor Nascimento <vyctor.nsacimento1@gmail.com>
2023-06-19 16:42:54 -0300

    Teste Git

--- project.py
+++ project.py
@@ -1,3 +1,9 @@
 print ("Olá mundo")
 mensagem = ("Olá mundo novamente")
 print (mensagem)
+
+# Subindo arquivos para um repositório
+
+print ("Teste")
+EU_SOU_LINDO =("O Gabriel é feio")
+print (EU_SOU_LINDO)
