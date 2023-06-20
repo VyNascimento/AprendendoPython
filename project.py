0c465a140e337d3930d9a148db233893a47ffdeb
Vyctor Nascimento <vyctor.nsacimento1@gmail.com>
2023-06-20 15:36:03 -0300

    testando comandos git

--- project.py
+++ project.py
@@ -7,3 +7,11 @@ print (mensagem)
 print ("Teste")
 EU_SOU_LINDO =("O Gabriel é feio")
 print (EU_SOU_LINDO)
+
+#comandos git
+
+valor_do_produto = 50
+valor_de_venda = 150
+lucro = valor_de_venda - valor_do_produto
+
+print (lucro)
